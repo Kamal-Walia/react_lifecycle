7cce46824b605cb516442e3eff12f96960f329ef
Kamal Walia <kamalw1709@gmail.com>
2022-09-27 20:19:33 +0530

    LifeCycle Methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import LifeCycleMethods from './LifeCycleMethods';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      removeComponent: false
+    }
+  }
+  render(){
+    return(
+      <div>
+      {!this.state.removeComponent && <LifeCycleMethods /> } 
+      <button onClick={() => this.setState({removeComponent: !this.state.removeComponent})}>Toggle Component</button>
+      </div>
+    )
+  }
+
+
 }
+
 
 export default App;
